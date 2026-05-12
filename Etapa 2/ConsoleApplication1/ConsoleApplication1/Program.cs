2e4bf98b742344d9ec4d66adfab9af6c3ca63e16
Evelyn Escobar <eveesscobar8@gmail.com>
2026-05-12 08:27:55 -0300

    subo etapa 1 y 2

---
+++ Etapa 2/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numeros = { 200, 150, 100, -50, 300 };// Un array de 5 números enteros
+            int suma; // Un entero que será su suma
+            suma = numeros[0] + numeros[1] + numeros[2] + numeros[3] + numeros[4]; // Hallamos la suma
+            Console.WriteLine("Su suma es {0}", suma);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Etapa 2/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vehiculos = 0;
+            int contador_recaudacion = 0;
+            int opcion;
+
+            do
+            {
+                Console.WriteLine(" 1 ingresar vehiculo");
+                Console.WriteLine(" 2 retirar vehiculo");
+                Console.WriteLine(" 3 estado");
+                Console.WriteLine(" 4 salirr");
+
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+
+                        if (vehiculos == 20)
+                        {
+                            Console.WriteLine("estacionam lleno");
+                        }
+                        else
+                        {
+                            vehiculos++;
+                        }
+
+                        break;
+                    case 2:
+                        if (vehiculos <= 0)
+                        {
+                            Console.WriteLine("no hay");
+                        }
+                        else
+                        {
+                            Console.WriteLine("ingr pago");
+                            int pago = int.Parse(Console.ReadLine());
+
+                            if (pago < 2500)
+                                Console.WriteLine("insuficien");
+                            else
+                            {
+                                vehiculos--;
+                                contador_recaudacion = contador_recaudacion + 2500;
+                                Console.WriteLine("vehiculo retirado");
+                            }
+
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("vehiculo" + vehiculos);
+
+                        break;
+                    case 4:
+                        Console.WriteLine("sistema cerrado");
+                        break;
+
+                    default:
+
+                        Console.WriteLine("op invalid");
+                        break;
+                }
+
+            }
+            while (opcion != 4);
+            Console.ReadKey();
+        }
+    }
+}

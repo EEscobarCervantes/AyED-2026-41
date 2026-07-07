e05527a980fa42c4780f899b13374b4d9dd3c540
Escobar Evelyn <eveesscobar8@gmail.com>
2026-07-07 08:43:38 -0300

    subo unidades 1,2 y 3

--- Etapa 3/11_CalificacionesEstudiantiles/11_CalificacionesEstudiantiles/Program.cs
+++ Etapa 3/11_CalificacionesEstudiantiles/11_CalificacionesEstudiantiles/Program.cs
@@ -10,6 +10,30 @@ namespace _11_CalificacionesEstudiantiles
     {
         static void Main(string[] args)
         {
+            Console.Write("ingrese la cantidad de filas y columnas: ");
+            int n = int.Parse(Console.ReadLine());
+            string[,] estudiantes = new string [n, 3];
+            for (int i=0; i < n; i++)
+            {
+                Console.WriteLine("estudiante" + (i + 1));
+                Console.Write("Nombre: ");
+                estudiantes[i, 0] = Console.ReadLine();
+                Console.Write("Edad: ");
+                estudiantes[i, 1] = Console.ReadLine();
+                Console.Write("Calificación: ");
+                estudiantes[i, 2] = Console.ReadLine();
+            }
+            Console.WriteLine("------Lista de estudiantes------");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Nombre: " + estudiantes[i, 0] +" | Edad: " + estudiantes[i, 1] +" | Calificación: " + estudiantes[i, 2]);
+            }
+            Console.ReadKey();
+
+
+
+
+
         }
     }
 }
